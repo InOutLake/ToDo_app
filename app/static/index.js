cc8a6a2bcc8cd0134df1e66a2149868d3ed34acc
InOutLake <evpatcolovratez@gmail.com>
2024-11-18 09:54:19 +0700

    all besides create

--- app/static/index.js
+++ app/static/index.js
@@ -1,28 +1,132 @@
-const taskCardsContainers = document.querySelectorAll('.task-cards');
-let taskCards = document.querySelectorAll('.task-card');
+async function fetchTasks() {
+  try {
+    const response = await fetch('/tasks/');
+    const tasks = await response.json();
+    
+    taskCardsContainers = document.querySelectorAll(".task-cards");
 
+    tasks.forEach(task => {
+      const taskCard = document.createElement('div');
+      taskCard.className = 'task-card';
+      taskCard.draggable = true;
+      taskCard.id = `task-card-${task.id}`;
+      
+      const taskTitle = document.createElement('h3');
+      taskTitle.textContent = task.name;
+      taskCard.appendChild(taskTitle);
+      
+      const taskDescription = document.createElement('p');
+      taskDescription.textContent = task.description;
+      taskCard.appendChild(taskDescription);
+      
+      // TODO rework this using enum
+      if (task.status === "To Do") {
+        taskCardsContainers[0].appendChild(taskCard);
+      } else if (task.status === "In Progress") {
+        taskCardsContainers[1].appendChild(taskCard);
+      } else if (task.status === "Completed"){
+        taskCardsContainers[2].appendChild(taskCard);
+      } else{
+        throw new Error('Unexpected task status');
+      }
+      
+      taskCard.addEventListener("dragstart", dragstartHandler);
+      taskCard.addEventListener("dragend", dragendHandler);
+      taskCard.addEventListener("dblclick", dbclickHandler);
+    });
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+  }
+}
+
+const trashbox = document.getElementById("trash-dropbox");
+
+window.addEventListener("DOMContentLoaded", fetchTasks);
+
+// --- Drag event ---
 function dragstartHandler(ev) {
   ev.dataTransfer.setData("text/plain", ev.target.id);
   ev.dataTransfer.dropEffect = "move";
+  trashbox.classList.add('show');
 }
-
-window.addEventListener("DOMContentLoaded", () => {
-  for (const taskCard of taskCards) {
-    taskCard.addEventListener("dragstart", dragstartHandler);
-  }
-});
 
 function dragoverHandler(ev) {
   ev.preventDefault();
   ev.dataTransfer.dropEffect = "move";
 }
 
-function dropHandler(ev) {
+async function dropHandler(ev) {
   ev.preventDefault();
   const data = ev.dataTransfer.getData("text/plain");
-  const taskCard = document.getElementById(data);
-  
-  if (taskCardsContainers.includes(ev.target)) {
-    ev.target.appendChild(taskCard);
+  const taskCard = [...document.querySelectorAll(".task-card")].find(element => element.id === data);
+
+  if (ev.target.id === "trash-dropbox" || ev.target.parentNode.id === "trash-dropbox") {
+    const taskId = taskCard.id.split('-')[2];
+    const response = await fetch(`/tasks/${taskId}`, {
+      method: 'DELETE'
+    })
+    if (response.ok) {
+      taskCard.remove();
+    } else {
+      alert('Unable to delete task. Please check the server logs for more information.');
+    }
+    return;
   }
+  else {
+    let dropElem = ev.target.closest('.task-cards');
+    const taskId = taskCard.id.split('-')[2];
+    const response = await fetch(`/tasks/${taskId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ status: ev.target.closest('.task-column').id })
+    })
+
+    if (dropElem && response.ok) {
+      dropElem.appendChild(taskCard);
+    } else {
+      alert('Unable to move task. Please check the server logs for more information.');
+    }
+  }
+}
+
+async function dragendHandler(ev) {
+  ev.preventDefault();
+  trashbox.classList.remove('show');
+}
+
+
+// --- Edit event ---
+async function dbclickHandler(ev) {
+  const toChange = ev.target.closest('.task-card');
+  if (toChange.getAttribute('contenteditable') === 'true') return;
+  toChange.setAttribute('contenteditable', 'true');
+  toChange.setAttribute('draggable', 'false');
+  toChange.focus();
+  toChange.addEventListener('blur', async () => {
+    if (toChange.getAttribute('contenteditable') === 'true') {
+      const name = toChange.children[0].textContent;
+      const description = toChange.children[1].textContent;
+      const response = await fetch(`/tasks/${toChange.id.split('-')[2]}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: name, description: description })
+      });
+
+      if (response.ok) {
+        toChange.removeAttribute('contenteditable');
+        toChange.setAttribute('draggable', 'true');
+      } else {
+        alert('Unable to save changes. Please check the server logs for more information.');
+      }
+    }
+  });
+}
+
+// --- Create event ---
+async function createTask() {
+  
 }
